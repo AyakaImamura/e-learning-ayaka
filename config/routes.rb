54533e0ea916a4a5380efeac8689f58f2f0ef871
Ayaka Imamura <aykcw.724@gmail.com>
2020-02-10 09:13:16 +0800

    [Layout]Create Sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-    get 'users/new'
-    root 'static_pages#home'
+  root 'static_pages#home'
+  
+  resources :users
+  get '/signup', to: 'users#new'
+  # resources :usersと入力した瞬間に、new.htmlに飛べるようになる。
+  # その上で、5行目は、URLを/signupと設定し、そこに飛ばすとusers#newというページにリンクするというカスタマイズ
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
