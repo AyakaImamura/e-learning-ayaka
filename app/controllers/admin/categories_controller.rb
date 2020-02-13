c21dc84839263a3f649566317eba899a5492df2d
AyakaImamura <60721149+AyakaImamura@users.noreply.github.com>
2020-02-13 11:43:53 +0800

    [Function] Create Category (#9)

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,23 @@
 class Admin::CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def index
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
+
 end
