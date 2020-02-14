3ba0a4e951bf405a7f1adb3ded55696442c9ae8d
Ayaka Imamura <aykcw.724@gmail.com>
2020-02-14 12:28:21 +0800

    [Function] Update and Delete Category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -22,7 +22,17 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to admin_categories_url
+    else
+      render 'edit'
+    end
+  end
 
+  def destroy
+    Category.find(params[:id]).destroy
+    redirect_to admin_categories_url
   end
 
   private
