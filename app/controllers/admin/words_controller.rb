b6c19b24a7a4a1f817d90fa903a6d6b34513a20b
Ayaka Imamura <aykcw.724@gmail.com>
2020-02-18 12:14:23 +0800

    [Function]Admin pages for WORDS OPTION AND ANSWERS

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,5 +1,23 @@
 class Admin::WordsController < ApplicationController
   def new
-    @word = Word.new
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new #左記は省略系。Word.new(category_id: params[:category_id])
+    3.times{ @word.choices.new}
   end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new(word_params)
+    if @word.save
+      redirect_to admin_category_url(@category)
+    else
+      render 'new'
+    end
+  end
+
+  private #定義するためだけに存在するのでprivate
+  def word_params
+    params.require(:word).permit(:content, choices_attributes: [:id, :content, :is_correct])
+  end
+  
 end
