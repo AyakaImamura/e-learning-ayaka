c9bd8f3f2a9ac57743ee94b0a07524cbf39e327f
Ayaka Imamura <aykcw.724@gmail.com>
2020-02-28 10:37:38 +0800

    [Function]Activity feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,8 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  has_many :activities
+
   def follow(user)
     active_relationships.create(followed: user)
   end
