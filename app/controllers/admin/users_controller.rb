6f5d473c99a15c211a04b6c3982f1ffed8a8a560
Ayaka Imamura <aykcw.724@gmail.com>
2020-03-03 11:41:34 +0800

    [Function] Admin Users Management

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,22 @@
 class Admin::UsersController < ApplicationController
+  before_action :check_admin
+
   def index
     @users = User.paginate(page: params[:page], per_page:10)
   end
+
+  def update #add user as admin
+    @user = User.find(params[:id])
+    @user.update_attribute(:is_admin, true)
+
+    redirect_to admin_users_url
+  end
+
+  def destroy #remove user as admin
+    @user = User.find(params[:id])
+    @user.update_attribute(:is_admin, false)
+
+    redirect_to admin_users_url
+  end
+
 end
